2fb9db7ea240cb0b4a1440f1301074d51ba7ba06
Reyan <imreyanoumer@gmail.com>
2024-09-02 15:50:08 +0300

    it's just getting started :)

---
+++ PiParent/user/serializers.py
@@ -0,0 +1,9 @@
+from djoser.serializers import UserSerializer as BaseUserSerializer,UserCreateSerializer as BaseUserCreateSerializer
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id','phone','password']
+        
+class UserSerializers(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        fields = ['id','phone','password','first_name']
