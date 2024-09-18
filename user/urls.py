2f0f990b13572d5ef154fa5c90fd203de1de9b0d
Ludis <leulsegedmelaku1020@gmail.com>
2024-09-18 13:13:09 +0300

    Update requirements.txt with django-phonenumber-field and pillow

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,7 @@ router.register('students', StudentViewSet)
 router.register('teachers', TeacherViewSet)
 router.register('subjects',SubjectViewSet)
 router.register('course-parent', ParentCourseRecommendationViewSet)   
-router.register('course-admin', CourseRecommendationViewSet)
+# router.register('course-admin', CourseRecommendationViewSet)
 router.register(r'missed-events', MissedEventViewSet)
 router.register('attendance', AttendanceViewSet)
 
